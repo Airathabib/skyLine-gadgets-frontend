fa5d5e839abe187818f1d569f8a6cba93b960132
Airat <airat.24@mail.ru>
2025-11-10 13:44:24 +0300

    fix_footer_css_styles

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -4,7 +4,9 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.Footer}>
       <div className={styles.FooterContainer}>
-        <p>© {new Date().getFullYear()} SkyLine Gadget. Все права защищены.</p>
+        <p className={styles.FooterText}>
+          © {new Date().getFullYear()} SkyLine Gadget. Все права защищены.
+        </p>
       </div>
     </footer>
   );
